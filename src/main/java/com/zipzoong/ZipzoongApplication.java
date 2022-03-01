312ba80adfba6d930b10d5373404f353d7b9036c
hyung <bigbluepado@gmail.com>
2022-03-01 11:03:23 +0900

    first commit

---
+++ src/main/java/com/zipzoong/ZipzoongApplication.java
@@ -0,0 +1,13 @@
+package com.zipzoong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipzoongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipzoongApplication.class, args);
+    }
+
+}
