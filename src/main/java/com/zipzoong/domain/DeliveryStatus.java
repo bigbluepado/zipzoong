ff3722b7f457c98af743170390fdccd03dd67602
hyung <bigbluepado@gmail.com>
2022-03-04 08:49:44 +0900

    update all

---
+++ src/main/java/com/zipzoong/domain/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package com.zipzoong.domain;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
---
+++ src/main/java/com/zipzoong/repository/order/simplequery/OrderSimpleQueryDto.java
@@ -0,0 +1,25 @@
+package com.zipzoong.repository.order.simplequery;
+
+import com.zipzoong.domain.Address;
+import com.zipzoong.domain.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderSimpleQueryDto {
+
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate; //주문시간
+    private OrderStatus orderStatus;
+    private Address address;
+
+    public OrderSimpleQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+    }
+}
